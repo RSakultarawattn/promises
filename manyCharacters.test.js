8612f8a9533ac8b8f1259e90534ffcfdb6cb1bc3
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-03 14:06:25 -0800

    allbutmock

--- manyCharacters.test.js
+++ manyCharacters.test.js
@@ -1,9 +1,12 @@
 const fs = require('fs').promises
 const { manyCharacters } = require('./manyCharacters.js');
 
+
+
 describe('manyCharacters function', () => {
 
     it('returns a list of characters', async () => {
+
         const characters = await manyCharacters([1, 3, 5, 7]);
         expect(characters).toEqual([
             { name: 'Rick Sanchez', species: 'Human', status: 'Alive' },
